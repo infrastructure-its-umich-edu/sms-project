68e91f500c4e5e450b19748ff7418de9966a4733
Brian Awood <bawood@umich.edu>
2017-01-25 14:33:23 -0500

    use new sms utility function

--- sendsms/views.py
+++ sendsms/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404,render
 from django.http import HttpResponseRedirect
 
 from .forms import MessageForm
+from ..utils import twosmsMessage
 
+import logging
+
+logger = logging.getLogger(__name__)
+twosms = twosmsMessage()
+
+@login_required
 def get_message(request):
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if form.is_valid():
+            clean_number = form.cleaned_data['number']
+            clean_message = form.cleaned_data['message']
             # do something like send the message
             # then redirect to acknowledgement url
+            result = twosms.send( clean_number, clean_message )
+            logger.debug(result)
             return HttpResponseRedirect('/sms/send')
 
     else:
