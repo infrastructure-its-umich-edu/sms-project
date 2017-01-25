fc61e29a661596bbb07f54adc8861a7ea0abb940
Brian Awood <bawood@umich.edu>
2017-01-25 14:33:53 -0500

    get sms user and pass from environment file

--- sms/settings.py
+++ sms/settings.py
@@ -174,3 +174,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# value for SMS provider
+SMS_USER = config('SMS_USER')
+SMS_PASS = config('SMS_PASS')
