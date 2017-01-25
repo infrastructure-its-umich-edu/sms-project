8c68cb52dacece083bbb99ff438b22d427243d2c
Brian Awood <bawood@umich.edu>
2017-01-25 17:42:40 -0500

    use kitchen to convert cleaned unicode string

--- sendsms/views.py
+++ sendsms/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404,render
 from django.http import HttpResponseRedirect
+from kitchen.text.converters import to_bytes
 
 from .forms import MessageForm
 from utils import twosmsutils
@@ -15,11 +16,12 @@ def get_message(request):
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if form.is_valid():
-            clean_number = form.cleaned_data['number']
-            clean_message = form.cleaned_data['message']
+            clean_number = form.cleaned_data.get('number')
+            clean_message = form.cleaned_data.get('message')
             # do something like send the message
             # then redirect to acknowledgement url
-            result = messageclient.send( clean_number, clean_message )
+            result = messageclient.send( to_bytes(clean_number, encoding='ascii' ),
+                                         to_bytes(clean_message, encoding='ascii' ))
             logger.debug(result)
             return HttpResponseRedirect('/sms/send')
 
