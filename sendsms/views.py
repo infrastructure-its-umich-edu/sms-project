f1dfbc6487c724d33fad87d67027fd39334fa8a9
Brian Awood <bawood@umich.edu>
2017-01-25 16:03:40 -0500

    fixup package and module name imports

--- sendsms/views.py
+++ sendsms/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import get_object_or_404,render
 from django.http import HttpResponseRedirect
 
 from .forms import MessageForm
-from ..utils import twosmsMessage
+from utils import twosmsutils
 
 import logging
 
 logger = logging.getLogger(__name__)
-twosms = twosmsMessage()
+messageclient = twosmsutils.twosmsMessage()
 
 @login_required
 def get_message(request):
@@ -19,7 +19,7 @@ def get_message(request):
             clean_message = form.cleaned_data['message']
             # do something like send the message
             # then redirect to acknowledgement url
-            result = twosms.send( clean_number, clean_message )
+            result = messageclient.send( clean_number, clean_message )
             logger.debug(result)
             return HttpResponseRedirect('/sms/send')
 
