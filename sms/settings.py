0cb8a510112ab5cd17e36f072448554bbd84ed99
Brian Awood <bawood@umich.edu>
2017-02-07 14:34:16 -0500

    add STATIC_ROOT to collect static files

--- sms/settings.py
+++ sms/settings.py
@@ -196,6 +196,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = Path(BASE_DIR, 'static/')
 
 # value for SMS provider
 SMS_USER = config('SMS_USER')
