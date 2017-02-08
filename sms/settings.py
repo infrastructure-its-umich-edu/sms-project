8ca380d5e5081a62ede65c27182610375f74ff36
Brian Awood <bawood@umich.edu>
2017-02-08 11:20:50 -0500

    adjust STATIC_ROOT so it's not at /

--- sms/settings.py
+++ sms/settings.py
@@ -196,7 +196,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = Path(BASE_DIR, '/static/')
+STATIC_ROOT = Path(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
