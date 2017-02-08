17a2f10295ab8d42d320200dc712675769f5cdfe
Brian Awood <bawood@umich.edu>
2017-02-08 15:26:15 -0500

    switch to https 2sms url and add msg uuid

--- utils/twosmsutils.py
+++ utils/twosmsutils.py
@@ -1,13 +1,15 @@
 from django.conf import settings
 from zeep import Client
 from zeep import xsd
+import uuid
 
 class twosmsMessage:
     """A utility class for connecting with sms vendor 2sms"""
     sms_user = settings.SMS_USER
     sms_pass = settings.SMS_PASS
-    mc = Client('http://smsportal.2sms.com/WebServices/2.2/Simple/Message.asmx?WSDL')
+    mc = Client('https://smsportal.2sms.com/WebServices/2.2/Simple/Message.asmx?WSDL')
     messageClient = mc.bind('Message', 'MessageSoap12')
 
     def send(self, to, message):
-        return self.messageClient.Send( self.sms_user, self.sms_pass, '2', '1', to, message, 'NULL', 'umich2smstest', 'testing', 'NULL', '1', '1', '1', 'result', 'code' )
+        msgid = str(uuid.uuid1())
+        return self.messageClient.Send( self.sms_user, self.sms_pass, '2', '1', to, message, 'NULL', 'header', msgid, 'NULL', '1', '1', '1', 'result', 'code' )
