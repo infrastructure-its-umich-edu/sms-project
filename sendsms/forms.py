663c0a8b9f45e66055b54c68d03d34773b085bbe
Brian Awood <bawood@umich.edu>
2017-02-09 11:20:38 -0500

    2sms seems to only accept 10 digit number

--- sendsms/forms.py
+++ sendsms/forms.py
@@ -4,5 +4,5 @@ from django.core.validators import RegexValidator
 
 class MessageForm(forms.Form):
     number = forms.CharField(label='Phone Number',
-                             validators=[RegexValidator(regex='^[0-9]{10,11}$')])
+                             validators=[RegexValidator(regex='^[0-9]{10}$')])
     message = forms.CharField(label='Message', max_length=160)
