77e7d2380aa97973c7b9e2a353141b4fd8e6c8bd
Brian Awood <bawood@umich.edu>
2017-02-15 16:20:44 -0500

    improve validator regex for phone
    allow additional normal characters between number segments
    like ()-. and allow multiple numbers seperated by ;

--- sendsms/forms.py
+++ sendsms/forms.py
@@ -4,5 +4,5 @@ from django.core.validators import RegexValidator
 
 class MessageForm(forms.Form):
     number = forms.CharField(label='Phone Number',
-                             validators=[RegexValidator(regex='^[0-9]{10}$')])
+                             validators=[RegexValidator(regex='^(\(?([2-9][0-8][0-9])\)?[-. ]?([2-9][0-9]{2})[-. ]?([0-9]{4});?)+$')])
     message = forms.CharField(label='Message', max_length=160)
